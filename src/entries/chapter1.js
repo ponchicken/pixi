dd58c40af07b35065c7f9100a03dc7f6dc3e6b20
Alexander Baladin <my@maggit.ru>
2019-07-16 14:29:03 +0300

    Loading progress in console

--- src/entries/chapter1.js
+++ src/entries/chapter1.js
@@ -1,6 +1,14 @@
 import * as PIXI from 'pixi.js'
 import '../styles/main.css'
 
+const Sprite = PIXI.Sprite
+const Rectangle = PIXI.Rectangle
+const Texture = PIXI.Texture
+const BaseTexture = PIXI.BaseTexture
+
+const loader = new PIXI.Loader()
+const stage = new PIXI.Container()
+
 const renderer = PIXI.autoDetectRenderer({
   width: 256,
   height: 256
@@ -8,20 +16,16 @@ const renderer = PIXI.autoDetectRenderer({
 
 document.body.appendChild(renderer.view)
 
-const stage = new PIXI.Container()
-
 /**
  * loader
  */
-
-const loader = new PIXI.Loader()
 
 loader
   .add('cat', 'assets/cat.png')
   .add('catsTileset', 'assets/catsTileset.png')
   .load(() => {
     /** cat */
-    const cat = new PIXI.Sprite(
+    const cat = new Sprite(
       loader.resources.cat.texture
     )
 
@@ -33,13 +37,7 @@ loader
     stage.addChild(cat)
 
     /** tileset */
-    const catsSprite = new PIXI.Sprite(getFrame({
-      source: 'catsTileset',
-      x: 128,
-      y: 128,
-      width: 32,
-      height: 32
-    }))
+    const catsSprite = new Sprite(getFrame('catsTileset', 128, 128, 32, 32))
 
     catsSprite.position.set(64, 64)
 
@@ -49,24 +47,33 @@ loader
     renderer.render(stage)
   })
 
+loader.onLoad.add((loader, resource) => {
+  console.info({
+    progress: `${loader.progress}%`,
+    loading: resource.url
+  })
+})
+
+loader.onComplete.add(() => {
+  console.info('Completed!')
+})
+
 /** Frame creator */
-function getFrame ({
-  source, x, y, width, height
-}) {
+function getFrame (source, x, y, width, height) {
   let texture = null
 
   if (typeof source === 'string') {
     if (loader.resources[source]) {
       texture = loader.resources[source].texture
     }
-  } else if (source instanceof PIXI.BaseTexture) {
-    texture = new PIXI.Texture(source)
+  } else if (source instanceof BaseTexture) {
+    texture = new Texture(source)
   }
 
   if (!texture) {
     console.warn(`Load the ${source} texture into cache`)
   } else {
-    const frame = new PIXI.Rectangle(x, y, width, height)
+    const frame = new Rectangle(x, y, width, height)
     texture.frame = frame
     return texture
   }
