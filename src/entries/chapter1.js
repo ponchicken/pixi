c9e815adfde8a543a721d81ee8c690cff13c4a5d
Alexander Baladin <my@maggit.ru>
2019-07-16 11:21:22 +0300

    Load and render sprite

--- src/entries/chapter1.js
+++ src/entries/chapter1.js
@@ -7,3 +7,30 @@ const renderer = PIXI.autoDetectRenderer({
 })
 
 document.body.appendChild(renderer.view)
+
+const stage = new PIXI.Container()
+
+/**
+ * loader
+ */
+
+const loader = new PIXI.Loader()
+const sprites = {}
+
+loader
+  .add('cat', 'assets/cat.png')
+  .load(() => {
+    sprites.cat = new PIXI.Sprite(
+      loader.resources.cat.texture
+    )
+
+    stage.addChild(sprites.cat)
+    renderer.render(stage)
+  })
+
+// loader.onComplete
+//   .add(() => {
+//     console.log(sprites.cat)
+//     stage.addChild(sprites.cat)
+//     renderer.render(stage)
+//   })
